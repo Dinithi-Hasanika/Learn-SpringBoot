c78a38b111396d55348368995a8f38fc50ce7e86
dinithih <dinithiliyanage.95@gmail.com>
2023-03-09 18:57:17 +0530

    Add user entity and repo

---
+++ src/main/java/com/example/demo/entity/User.java
@@ -0,0 +1,65 @@
+package com.example.demo.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue
+    private int id;
+
+    @Column(name="username")
+    private String userName;
+
+    @Column(name="firstname")
+    private String firstName;
+
+    @Column(name = "lastname")
+    private String lastName;
+
+    public int getId() {
+
+        return id;
+    }
+
+    public void setId(int id) {
+
+        this.id = id;
+    }
+
+    public String getUserName() {
+
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+
+        this.userName = userName;
+    }
+
+    public String getFirstName() {
+
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+
+        this.lastName = lastName;
+    }
+}
